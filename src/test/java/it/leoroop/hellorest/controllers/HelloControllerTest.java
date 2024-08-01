6d4e0d7e5bdeb16600e22765b3567590a74c1f5e
leoroop <leon.berta@gmail.com>
2024-08-01 22:28:25 +0200

    Added empty unit test

---
+++ src/test/java/it/leoroop/hellorest/controllers/HelloControllerTest.java
@@ -0,0 +1,12 @@
+package it.leoroop.hellorest.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class HelloControllerTest {
+    @Test
+    public void testHello() {
+        fail(); //TODO: Implement test
+    }
+}
