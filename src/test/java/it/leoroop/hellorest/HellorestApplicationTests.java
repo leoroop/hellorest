845f21b43ac8c8d0f6a2ffd55a9c86dd66cd75d5
leoroop <leon.berta@gmail.com>
2024-08-01 22:37:01 +0200

    Removed failing test from HellorestApplicationTests, will add pertinent tests later

--- src/test/java/it/leoroop/hellorest/HellorestApplicationTests.java
+++ src/test/java/it/leoroop/hellorest/HellorestApplicationTests.java
@@ -5,12 +5,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
+// Test class added ONLY to cover main() invocation not covered by application tests.
 @SpringBootTest
 class HellorestApplicationTests {
 
 	@Test
 	void contextLoads() {
-		fail();
 	}
 
 }
